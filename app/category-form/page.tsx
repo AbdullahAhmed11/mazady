141c21909e87f3da550e9da403f3129e6b2ac2f8
AbdullahAhmed11 <abdullah77zzx@gmail.com>
2025-03-09 05:46:20 +0200

    add update

---
+++ app/category-form/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CategoryForm from '../components/CategoryForm'
+const page = () => {
+  return (
+    <div className="max-w-2xl mx-auto mt-5">
+    <h1 className="text-3xl font-bold text-center mb-8">Category Selection</h1>
+    <CategoryForm />
+  </div>
+  )
+}
+
+export default page
